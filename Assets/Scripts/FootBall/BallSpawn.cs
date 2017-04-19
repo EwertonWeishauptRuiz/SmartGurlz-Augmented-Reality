0d37c83bf1c0768637c08a11c2b1057f68107a9c
Ewerton <shakamaiden@gmail.com>
2017-04-19 11:51:02 +0200

    Tweaks in Balls and trees

---
+++ Assets/Scripts/FootBall/BallSpawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawn : MonoBehaviour {
+
+    public Transform ballStart;
+    public Transform ballEnd;
+
+    public GameObject[] ballPrefab;
+    public GameObject[] ballClone;
+    // Update is called once per frame
+
+    public float spawnTimer = 3;
+	void Update () {
+        if(spawnTimer <= 0) {
+            SpawnBall();
+            spawnTimer = 3;
+        }
+        spawnTimer -= Time.deltaTime;
+    }	
+    void SpawnBall() {
+        ballClone[0] = Instantiate(ballPrefab[0], ballStart.transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;       
+    }
+
+}
